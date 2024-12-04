578bf3cdb1c19f025fd0f08e0c3f01f144db3a17
GuilhemHnr <guilhem.hnr@gmail.com>
2024-12-04 18:10:28 +0100

    corrected clean up script

--- Task_01/clean.sh
+++ Task_01/clean.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-# Define the path to the fullstackapp repository (assuming it is in the current directory)
-PROJECT_DIR=./fullstackapp
+PROJECT_DIR=~/M2_Virtualization/PROJECT/VirtuProject/Task_01/fullstackapp
 
-# Step 1: Cleanup Kubernetes resources
 echo "Deleting Kubernetes resources..."
 
 cd "$PROJECT_DIR/kubernetes"
 
-# Delete the frontend, backend, and database deployments
 kubectl delete -f frontend.yaml
 kubectl delete -f backend.yaml
 kubectl delete -f database.yaml
@@ -18,7 +15,7 @@ kubectl delete -f database.yaml
 echo "Stopping Minikube..."
 minikube stop
 
-#echo "Deleting Minikube's cluster..."
-#minikube delete
+echo "Deleting Minikube's cluster..."
+minikube delete
 
 echo "Cleanup completed successfully!"
